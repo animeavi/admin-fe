85ab42bce0fdd4125f1b1519b2d73c15fa978c5b
Angelina Filippova <linakirsanova@gmail.com>
2019-10-04 21:06:01 +0300

    Add test for rendering user profile

--- test/views/users/store.conf.js
+++ test/views/users/store.conf.js
@@ -1,5 +1,6 @@
 import app from '@/store/modules/app'
 import user from '@/store/modules/user'
+import userProfile from '@/store/modules/userProfile'
 import users from '@/store/modules/users'
 import getters from '@/store/getters'
 
@@ -7,6 +8,7 @@ export default {
   modules: {
     app,
     user,
+    userProfile,
     users
   },
   getters
