7440c2cef484aa4d912e6f8d2b6c61cad85eabf9
Angelina Filippova <linakirsanova@gmail.com>
2020-02-05 21:45:59 +0300

    Render select component for settings that have dropdown type

--- src/views/settings/components/Inputs.vue
+++ src/views/settings/components/Inputs.vue
@@ -24,7 +24,7 @@
       class="top-margin"
       @change="update($event, settingGroup.group, settingGroup.key, settingParent, setting.key, setting.type, nested)"/>
     <el-select
-      v-if="setting.type === 'module' || (setting.type.includes('atom') && setting.type.includes(false))"
+      v-if="setting.type === 'module' || (setting.type.includes('atom') && setting.type.includes('dropdown'))"
       :value="inputValue"
       clearable
       @change="update($event, settingGroup.group, settingGroup.key, settingParent, setting.key, setting.type, nested)">
