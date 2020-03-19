f4df449f1a4a2159461f6dec6a79014ab479509d
Angelina Filippova <linakirsanova@gmail.com>
2020-03-19 23:06:23 +0300

    Check if tabs is changed when search value was selected

--- test/views/settings/index.test.js
+++ test/views/settings/index.test.js
@@ -2,9 +2,13 @@ import Vuex from 'vuex'
 import { mount, createLocalVue, config } from '@vue/test-utils'
 import Element from 'element-ui'
 import Settings from '@/views/settings/index'
-import storeConfig from './store.conf'
-import { cloneDeep } from 'lodash'
 import flushPromises from 'flush-promises'
+import app from '@/store/modules/app'
+import getters from '@/store/getters'
+import relays from '@/store/modules/relays'
+import settings from '@/store/modules/settings'
+import user from '@/store/modules/user'
+import users from '@/store/modules/users'
 
 config.mocks["$t"] = () => {}
 
@@ -14,9 +18,21 @@ localVue.use(Element)
 
 describe('Settings search', () => {
   let store
+  let actions
 
   beforeEach(() => {
-    store = new Vuex.Store(cloneDeep(storeConfig))
+    actions = { SetActiveTab: jest.fn() }
+
+    store = new Vuex.Store({
+      modules: {
+        app,
+        relays,
+        settings: { ...settings, actions },
+        user,
+        users
+      },
+      getters
+    })
   })
 
   it('shows search input', async (done) => {
@@ -28,6 +44,17 @@ describe('Settings search', () => {
     await flushPromises()
     const searchInput = wrapper.find('.settings-search-input')
     expect(searchInput.exists()).toBe(true)
+    done()
+  })
+
+  it('changes tab when search value was selected', async (done) => {
+    const wrapper = mount(Settings, {
+      store,
+      localVue
+    })
+    await flushPromises()
+    wrapper.vm.handleSearchSelect({ group: 'Pleroma.Upload', key: 'Pleroma.Upload' })
+    expect(actions.SetActiveTab).toHaveBeenCalled()
     done()
   })
 })
