54e32f7ad6cee6122d73538ac51bf4fb4715c40d
Mark Felder <feld@FreeBSD.org>
2020-03-31 11:59:48 -0500

    Leave emoji packs disabled in master branch for now.
    This ensures a build doesn't get accidentally produced with it enabled.

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
   BASE_API: '"https://api-prod"',
-  DISABLED_FEATURES: '[""]',
+  DISABLED_FEATURES: '["emoji-packs"]',
   ASSETS_PUBLIC_PATH: '/pleroma/admin/'
 }
