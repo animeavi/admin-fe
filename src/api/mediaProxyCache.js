0aff86e6384d8e70dbdb26d441aae701ed5fd988
Angelina Filippova <linakirsanova@gmail.com>
2020-07-03 03:04:17 +0300

    Create API functions for MediaProxy Cache

---
+++ src/api/mediaProxyCache.js
@@ -0,0 +1,34 @@
+import request from '@/utils/request'
+import { getToken } from '@/utils/auth'
+import { baseName } from './utils'
+
+export async function listBannedUrls(page, authHost, token) {
+  return await request({
+    baseURL: baseName(authHost),
+    url: `/api/pleroma/admin/media_proxy_caches?page=${page}`,
+    method: 'get',
+    headers: authHeaders(token)
+  })
+}
+
+export async function purgeUrls(urls, ban, authHost, token) {
+  return await request({
+    baseURL: baseName(authHost),
+    url: `/api/pleroma/admin/media_proxy_caches/purge`,
+    method: 'post',
+    headers: authHeaders(token),
+    data: { urls, ban }
+  })
+}
+
+export async function removeBannedUrls(urls, authHost, token) {
+  return await request({
+    baseURL: baseName(authHost),
+    url: `/api/pleroma/admin/media_proxy_caches/delete`,
+    method: 'post',
+    headers: authHeaders(token),
+    data: { urls }
+  })
+}
+
+const authHeaders = (token) => token ? { 'Authorization': `Bearer ${getToken()}` } : {}
