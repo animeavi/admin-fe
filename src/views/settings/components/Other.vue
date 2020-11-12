050134bfbf499259e8e3867bde2a270b706deb45
Angelina Filippova <linakirsanova@gmail.com>
2020-11-12 21:39:31 +0300

    Add User Backup settings, fix key for Pleroma.Web.Plugs.RemoteIp

--- src/views/settings/components/Other.vue
+++ src/views/settings/components/Other.vue
@@ -2,6 +2,10 @@
   <div v-if="!loading" :class="isSidebarOpen" class="form-container">
     <editor-input v-model="termsOfServicesContent" :name="'terms-of-service'" @input="handleEditorUpdate"/>
     <el-divider class="divider thick-line"/>
+    <el-form :model="backupData" :label-position="labelPosition" :label-width="labelWidth">
+      <setting :setting-group="backup" :data="backupData"/>
+    </el-form>
+    <el-divider v-if="backup" class="divider thick-line"/>
     <el-form :model="mimeTypesData" :label-position="labelPosition" :label-width="labelWidth">
       <setting :setting-group="mimeTypes" :data="mimeTypesData"/>
     </el-form>
@@ -42,6 +46,12 @@ export default {
     ...mapGetters([
       'settings'
     ]),
+    backup() {
+      return this.settings.description.find(setting => setting.key === 'Pleroma.User.Backup')
+    },
+    backupData() {
+      return _.get(this.settings.settings, [':pleroma', 'Pleroma.User.Backup']) || {}
+    },
     castAndValidate() {
       return this.settings.description.find(setting => setting.key === 'Pleroma.Web.ApiSpec.CastAndValidate')
     },
